4c223d47ffb7eb9298558e1bc007c67404acfd42
Matei Stefanescu <matei.stefanescu@mrm.com>
2023-12-31 14:40:06 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,188 +1,9 @@
 use std::env;
 use std::io;
 use std::process;
+use crate::matcher::Pattern;
 
-struct Pattern<'a> {
-    pattern: &'a str,
-    pattern_pos: usize,
-    input_pos: usize
-}
-
-impl Pattern<'_> {
-    fn advance(&mut self, input_line: &str, match_end: bool) -> bool {
-        self.pattern_pos += 1;
-        self.input_pos += 1;
-        return self.match_string(input_line, match_end);
-    }
-
-    fn reset(&mut self) {
-        self.input_pos = 0;
-        self.pattern_pos = 0;
-    }
-
-    fn match_string(&mut self, input_line: &str, match_end: bool) -> bool {
-        //check for $ condition
-        if match_end && self.input_pos < input_line.chars().count() && self.pattern_pos >= self.pattern.chars().count() {
-            return false;
-        }
-
-        if self.pattern_pos >= self.pattern.chars().count() {
-            return true;
-        }
-        if self.input_pos >= input_line.chars().count() && self.pattern_pos < self.pattern.chars().count() {
-            //edge case: if last parts of pattern are just "*" or "?" then it's match
-            let mut non_wildcards = false;
-            let mut pos = self.pattern_pos;
-            while pos < self.pattern.chars().count() && !non_wildcards {
-                if !['?'].contains(&self.pattern.chars().nth(pos).unwrap()) { //planned to be ['?', '*'].contains() but not requirement yet
-                    non_wildcards = true;
-                }
-                pos += 1;
-            }
-            println!("Reached end of input ({}) and not end of pattern ({}). Remaining all wildcards: {}", self.input_pos, self.pattern_pos, !non_wildcards);
-            return !non_wildcards;
-        }
-
-        let pattern_c = self.pattern.chars().nth(self.pattern_pos).unwrap();
-        let input_c = input_line.chars().nth(self.input_pos).unwrap();
-
-        if pattern_c == '\\' {
-            if self.pattern.chars().count() - 1 > self.pattern_pos {
-                match self.pattern.chars().nth(self.pattern_pos + 1).unwrap() {
-                    'd' => {
-                        if input_c.is_digit(10) {
-                            self.pattern_pos += 1;
-                            println!("Match {} with \\d", input_c);
-                            return self.advance(input_line, match_end);
-                        }
-                        println!("Character at position {} ({}) is not a digit", self.input_pos, input_c);
-                        return false;
-                    },
-                    'w' => {
-                        if input_c.is_digit(10) || input_c.is_alphabetic() {
-                            self.pattern_pos += 1;
-                            println!("Match {} with \\w", input_c);
-                            return self.advance(input_line, match_end);
-                        }
-                        println!("Character at position {} ({}) is not a word", self.input_pos, input_c);
-                        return false;
-                    },
-                    '\\' => {
-                        if input_c != '\\' {
-                            return false;
-                        }
-                        println!("Match {} with \\", input_c);
-                        self.pattern_pos += 1;
-                        return self.advance(input_line, match_end);
-                    }
-                    _ => {
-                        panic!("Unhandled escape sequence \\{} in {}", self.pattern.chars().nth(self.pattern_pos + 1).unwrap(), self.pattern);
-                    }
-                }
-            } else {
-                panic!("Unterminated escape sequence: {}", self.pattern);
-            }
-        } else if pattern_c == '[' {
-            let mut group = String::new();
-            let mut closing_bracket = -1;
-            let mut is_negative_group = false;
-            let mut pos = self.pattern_pos + 1;
-            while pos < self.pattern.chars().count() && closing_bracket == -1 {
-                let gc = self.pattern.chars().nth(pos).unwrap();
-                if gc == ']' {
-                    closing_bracket = pos as i32;
-                } else if pos == self.pattern_pos + 1 && gc == '^' {
-                    is_negative_group = true;
-                } else {
-                    group.push(gc);
-                }
-                pos += 1;
-            }
-            if closing_bracket == -1 {
-                panic!("Unterminated group sequence in {}", self.pattern);
-            }
-            return if (is_negative_group && group.contains(input_c)) || (!is_negative_group && !group.contains(input_c)) {
-                println!("{}[{}] is not matching group {} - is_negative={}", input_c, self.input_pos, group, is_negative_group);
-                false
-            } else {
-                self.pattern_pos = closing_bracket as usize;
-                self.advance(input_line, match_end)
-            }
-        } else if pattern_c == '+' {
-            let prev_char = self.pattern.chars().nth(self.pattern_pos - 1).unwrap();
-            println!("+ detected, trying to match with {} input_pos is {}", prev_char, self.input_pos);
-            while input_line.chars().nth(self.input_pos).unwrap() == prev_char && self.input_pos < input_line.chars().count() - 1 {
-                println!("next -> input_pos is {}", self.input_pos);
-                self.input_pos += 1;
-            }
-            self.input_pos -= 1;
-            return self.advance(input_line, match_end);
-        } else if pattern_c == input_c || pattern_c == '.' {
-            println!("Match {}[{}] with {}[{}]", pattern_c, self.pattern_pos, input_c, self.input_pos);
-            return self.advance(input_line, match_end);
-        } else if pattern_c != input_c {
-            if pattern_c == '?' {
-                //if we are here, the prev pattern char - the optional - was matched, so we just ignore and advance
-                println!("Detected ? with prev match, just advance");
-                self.input_pos -= 1; //hold the input, skip just the pattern char
-                return self.advance(input_line, match_end);
-            }
-            else if self.pattern_pos < self.pattern.chars().count() - 1 && self.pattern.chars().nth(self.pattern_pos + 1).unwrap() == '?' {
-                // for the case abc? -> abdv where the prev optional was not matched
-                println!("Detected ? with non-prev match, skip & advance");
-                self.pattern_pos += 1; //skip the unmatched optional char and the "?" but the additional increment is in advance
-                self.input_pos -= 1; //hold the input, skip just the pattern char
-                return self.advance(input_line, match_end);
-            }
-            else {
-                println!("No match {}[{}] with {}[{}]", self.pattern.chars().nth(self.pattern_pos).unwrap(), self.pattern_pos, input_line.chars().nth(self.input_pos).unwrap(), self.input_pos);
-                return false;
-            }
-        }
-        //if we reached here, it's the end of the line and there was no return false so it's a match
-        println!("Reached end, input_pos={} pattern_pos={}", self.input_pos, self.pattern_pos);
-        return true;
-    }
-}
-
-fn match_pattern(input_line: &str, pattern_str: &str) -> bool {
-    if pattern_str.chars().count() > 0 {
-        let mut match_start = false;
-        let mut match_end = false;
-        let mut final_pattern = pattern_str.to_string();
-        if pattern_str.chars().nth(0).unwrap() == '^' {
-            match_start = true;
-            final_pattern = final_pattern[1..].to_string();
-        }
-        if pattern_str.chars().last().unwrap() == '$' {
-            match_end = true;
-            final_pattern = final_pattern[..final_pattern.chars().count()-1].to_string();
-        }
-
-        let mut pattern = Pattern {
-            pattern: &final_pattern,
-            pattern_pos: 0,
-            input_pos: 0
-        };
-        let mut start = 0;
-        let mut matched = false;
-
-        while start < input_line.chars().count() && ! matched {
-            let input = &input_line.to_string()[start..];
-            pattern.reset();
-            println!();
-            println!("Trying to match string '{}' with pattern '{}'", input, pattern_str);
-            matched = pattern.match_string(input, match_end);
-            if match_start {
-                return matched;
-            }
-            start += 1;
-        }
-        return matched;
-    } else {
-        panic!("Unhandled pattern: {}", pattern_str);
-    }
-}
+mod matcher;
 
 // Usage: echo <input_text> | your_grep.sh -E <pattern>
 fn main() {
@@ -199,7 +20,10 @@ fn main() {
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    if match_pattern(&input_line.trim(), &pattern) {
+    let pattern_matcher = Pattern::new(&pattern, true);
+    dbg!(&pattern_matcher);
+
+    if pattern_matcher.test(&input_line) {
         println!("Match!");
         process::exit(0)
     } else {
---
+++ src/matcher.rs
@@ -0,0 +1,277 @@
+#[derive(Debug)]
+pub struct Pattern<'a> {
+    matchers: Vec<Matcher<'a>>,
+    start_modifier: bool,
+    pattern_str: &'a str,
+    debug: bool
+}
+
+impl<'a> Pattern<'a> {
+    pub fn new(input: &'a str, debug: bool) -> Self {
+        let mut matchers: Vec<Matcher> = Vec::new();
+        let mut skip = 0;
+        let start_with = input.chars().nth(0).unwrap_or('a') == '^';
+
+        for i in 0..input.chars().count() {
+            if skip > 0 {
+                skip -= 1;
+                continue;
+            }
+            if i == 0 && start_with {
+                continue;
+            }
+            let (matcher, skip_s) = Matcher::new(&input, i);
+            skip = skip_s;
+            matchers.push(matcher);
+        }
+        Self {matchers, pattern_str: input, start_modifier: start_with, debug}
+    }
+
+    pub fn test(&self, input: &str) -> bool {
+        if self.matchers.len() == 0 {
+            return true;
+        }
+        let mut start_index = 0;
+        while start_index < input.chars().count() - 1 {
+            if self.do_test(&input[start_index..]) {
+                return true;
+            }
+            if self.start_modifier {
+                return false;
+            }
+            start_index += 1;
+        }
+        return false;
+    }
+
+    fn do_test(&self, input: &str) -> bool {
+        let mut input_pos = 0;
+        let mut pattern_pos = 0;
+
+        print_debug(&format!("Testing {} against {}", input, self.pattern_str), self.debug);
+
+        while input_pos < input.chars().count() && pattern_pos < self.matchers.len() {
+            let (result, to_advance) = self.matchers[pattern_pos].test(input, input_pos, self.debug);
+            if !result {
+                return false;
+            }
+            input_pos += to_advance;
+            pattern_pos += 1;
+        }
+        return true;
+    }
+}
+#[derive(Debug)]
+enum Matcher<'a> {
+    Literal(char),
+    Digit,
+    Word,
+    GroupPositive(&'a str),
+    GroupNegative(&'a str),
+    StartOfString,
+    EndOfString,
+    WildCard,
+    OneOrMore(char),
+    ZeroOrOne(char),
+    Skip
+}
+
+impl<'a> Matcher<'a> {
+    pub fn new(input: &'a str, pos: usize) -> (Self, usize) {
+        match input.chars().nth(pos) {
+            Some('^') => {
+                (Self::StartOfString, 0)
+            },
+            Some('$') => {
+                (Self::EndOfString, 0)
+            },
+            Some('\\') => {
+                match input.chars().nth(pos + 1) {
+                    Some('d') => {
+                        (Self::Digit, 1)
+                    },
+                    Some('w') => {
+                        (Self::Word, 1)
+                    },
+                    Some('\\') => {
+                        (Self::Literal('\\'), 1)
+                    },
+                    Some(_) => {
+                        panic!("Unrecognized escape sequence")
+                    },
+                    None => {
+                        panic!("Unterminated escape sequence")
+                    }
+                }
+            },
+            Some('+') => {
+                (Self::Skip, 0) //handled in generic Some(c) - normally we shouldn't reach this
+            },
+            Some('?') => (Self::Skip, 0), //handled in generic Some(c) - normally we shouldn't reach this
+            Some('.') => (Self::WildCard, 0),
+            Some('[') => {
+                if let Some(closing_bracket) = find_closing(input, ']', pos) {
+                    match input.chars().nth(pos + 1) {
+                        Some('^') => (Self::GroupNegative(&input[pos + 2..closing_bracket]), closing_bracket - pos),
+                        Some(_) => (Self::GroupPositive(&input[pos + 1..closing_bracket]), closing_bracket - pos),
+                        None => panic!("This cannot happen")
+                    }
+                }
+                else {
+                    panic!("Unclosed [");
+                }
+            },
+            Some(']') => (Self::Skip, 0),
+            Some(c) => {
+                match input.chars().nth(pos + 1) {
+                    Some('+') => (Self::OneOrMore(c), 1),
+                    Some('?') => (Self::ZeroOrOne(c), 1),
+                    Some(_) => (Self::Literal(c), 0),
+                    None => (Self::Literal(c), 0)
+                }
+            },
+            None => panic!("Could not compile pattern")
+        }
+    }
+
+    fn test(&self, input: &str, pos: usize, debug: bool) -> (bool, usize) {
+        match self {
+            Self::Literal(pc) => {
+                match input.chars().nth(pos) {
+                    Some(c) => {
+                        let result = pc == &c;
+                        print_debug(&format!("Match {} {} with {}", result, pc, c), debug);
+                        (result, 1)
+                    },
+                    None => (false, 0)
+                }
+            },
+            Self::Digit => {
+                match input.chars().nth(pos) {
+                    Some(c) => {
+                        let result = c.is_digit(10);
+                        print_debug(&format!("Match {} \\d with {}", result, c), debug);
+                        (result, 1)
+                    },
+                    None => (false, 0)
+                }
+            },
+            Self::Word => {
+                match input.chars().nth(pos) {
+                    Some(c) => {
+                        let result = c.is_alphabetic() || c.is_digit(10);
+                        print_debug(&format!("Match {} \\w with {}", result, c), debug);
+                        (result, 1)
+                    },
+                    None => {
+                        print_debug(&format!("No match for \\w at position {}",  pos), debug);
+                        (false, 0)
+                    }
+                }
+            },
+            Self::GroupPositive(group) => {
+                match input.chars().nth(pos) {
+                    Some(c) => {
+                        let result = group.contains(c);
+                        print_debug(&format!("Match {} [{}] with {}", result, group, c), debug);
+                        (result, 1)
+                    },
+                    None => {
+                        print_debug(&format!("No match for [{}] at position {}", group, pos), debug);
+                        (false, 0)
+                    }
+                }
+            },
+            Self::GroupNegative(group) => {
+                match input.chars().nth(pos) {
+                    Some(c) => {
+                        let result = !group.contains(c);
+                        print_debug(&format!("Match {} [^{}] with {}", result, group, c), debug);
+                        (result, 1)
+                    },
+                    None => {
+                        print_debug(&format!("No match for [^{}] at position {}", group, pos), debug);
+                        (false, 0)
+                    }
+                }
+            },
+            Self::EndOfString => {
+                // cats cat$
+                (pos >= input.chars().count() - 1, 1)
+            },
+            Self::WildCard => {
+                match input.chars().nth(pos) {
+                    Some(c) => {
+                        print_debug(&format!("Matched . with {}", c), debug);
+                        (true, 1)
+                    },
+                    None => {
+                        print_debug(&format!("No match for . at position {}", pos), debug);
+                        (false, 0)
+                    }
+                }
+            },
+            Self::OneOrMore(pc) => {
+                let mut matched = 0;
+                return match input.chars().nth(pos) {
+                    Some(c) => {
+                        if &c != pc {
+                            print_debug(&format!("Match false for {}+ with {}", pc, c), debug);
+                            return (false, 1)
+                        }
+                        let mut search_pos = pos;
+                        while search_pos < input.chars().count() && &input.chars().nth(search_pos).unwrap() == pc {
+                            search_pos += 1;
+                            matched += 1;
+                        }
+                        print_debug(&format!("Match true {}+ with {}x{}", pc, matched, pc), debug);
+                        (true, matched)
+                    },
+                    None => {
+                        print_debug(&format!("No match for {}+ at position {}", pc, pos), debug);
+                        (false, 0)
+                    }
+                }
+            },
+            Self::ZeroOrOne(pc) => {
+                //doa?g - dog
+                match input.chars().nth(pos) {
+                    Some(c) => {
+                        if pc == &c {
+                            print_debug(&format!("Optional match true {}? with {} ", pc, c), debug);
+                            (true, 1)
+                        }
+                        else {
+                            print_debug(&format!("Optional match false {}? with {} ", pc, c), debug);
+                            (true, 0)
+                        }
+                    },
+                    None => {
+                        print_debug(&format!("Optional match false {}? at end of input ", pc), debug);
+                        (true, 0)
+                    }
+                }
+            },
+            Self::Skip => {
+                print_debug(&format!("Skip at position {}", pos), debug);
+                (true, 0)
+            },
+            _ => panic!("Invalid pattern")
+        }
+    }
+}
+
+fn find_closing(input: &str, element: char, after: usize) -> Option<usize> {
+    for (i, _) in input.match_indices(element) {
+        if i > after {
+            return Some(i);
+        }
+    }
+    return None;
+}
+
+fn print_debug(message: &str, debug: bool) {
+    if debug {
+        println!("{}", message);
+    }
+}
