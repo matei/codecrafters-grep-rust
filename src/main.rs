95d99d32a1b2f836e1646825b47e4e34a223415d
Matei Stefanescu <matei.stefanescu@mrm.com>
2024-01-08 11:14:43 +0200

    fix logs

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,15 @@ mod matcher;
 
 // Usage: echo <input_text> | your_grep.sh -E <pattern>
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-    pretty_env_logger::init();
-
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
         process::exit(1);
     }
-
     let debug = env::args().count() == 4 && env::args().nth(3).unwrap() == "-d";
     if debug {
         std::env::set_var("RUST_LOG", "trace");
     }
-
+    pretty_env_logger::init();
     let pattern = String::from(env::args().nth(2).unwrap().trim());
     let mut input_line = String::new();
 
